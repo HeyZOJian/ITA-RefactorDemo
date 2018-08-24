68f5c3717dc79f0e430403c6b3e66e0202d9eaf5
oocl-vito <hellozojian@gmail.com>
2018-08-24 14:09:03 +0800

    Extraction movie amount switch to Rental

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -23,48 +23,29 @@ public class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Enumeration rentals = this.rentals.elements();
-		String result = headerString();
+		StringBuilder result = new StringBuilder(headerString());
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-
-			switch (each.getMovie().getPriceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (each.getDayRented() > 2) {
-						thisAmount += (each.getDayRented() - 2) * 1.5;
-					}
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += each.getDayRented() * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDayRented() > 3) {
-						thisAmount += (each.getDayRented() - 3) * 1.5;
-					}
-					break;
-			}
-
+			thisAmount += each.getAmount();
 			//add frequent renter points
 			frequentRenterPoints++;
 			//add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1) {
 				frequentRenterPoints++;
 			}
-
 			//show figures for this rental
-			result += eachRentalString(thisAmount, each);
+			result.append(eachRentalString(thisAmount, each));
 			totalAmount += thisAmount;
 		}
 
-		result += footerString(totalAmount, frequentRenterPoints);
-		return result;
+		result.append(footerString(totalAmount, frequentRenterPoints));
+		return result.toString();
 	}
 
 	private String footerString(double totalAmount, int frequentRenterPoints) {
-		return "Amount owed is" + String.valueOf(totalAmount) + "\n"
-		+ "You earned" + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		return "Amount owed is " + String.valueOf(totalAmount) + "\n"
+		+ "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 	}
 
 	private String eachRentalString(double thisAmount, Rental each) {
