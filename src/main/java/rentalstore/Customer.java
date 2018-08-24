681d63cbb9f938e3d500e56960e958b7bff74a90
oocl-vito <hellozojian@gmail.com>
2018-08-24 14:13:58 +0800

    Extraction method that update frequentRenterPoints

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -23,24 +23,32 @@ public class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Enumeration rentals = this.rentals.elements();
+		// header
 		StringBuilder result = new StringBuilder(headerString());
+		// content
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			thisAmount += each.getAmount();
 			//add frequent renter points
-			frequentRenterPoints++;
-			//add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1) {
-				frequentRenterPoints++;
-			}
+			frequentRenterPoints = updateFrequentRenterPoints(frequentRenterPoints, each);
 			//show figures for this rental
 			result.append(eachRentalString(thisAmount, each));
 			totalAmount += thisAmount;
 		}
-
+		//footer
 		result.append(footerString(totalAmount, frequentRenterPoints));
+
 		return result.toString();
+	}
+
+	private int updateFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+		frequentRenterPoints++;
+		//add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1) {
+			frequentRenterPoints++;
+		}
+		return frequentRenterPoints;
 	}
 
 	private String footerString(double totalAmount, int frequentRenterPoints) {
